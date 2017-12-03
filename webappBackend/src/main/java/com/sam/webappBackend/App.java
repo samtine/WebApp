9427691b5be50e270a0d270920ca0f36425e4d46
samtine <samtinesp@gmail.com>
2017-12-03 08:12:26 +0530

    My project initiation

---
+++ webappBackend/src/main/java/com/sam/webappBackend/App.java
@@ -0,0 +1,13 @@
+package com.sam.webappBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
